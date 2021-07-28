dbf5e47f4a1b39d19c8997afdb0763a451c134b6
Piotr Moroń <noperekq@gmail.com>
2021-07-28 16:35:09 +0200

    asd

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var products = {
+    1: {
+      name : "Pizza veloper",
+      desc : "Lorem ipsum dolor sit amet, constetur",
+      img : "pizza-veloper.svg",
+      price : 150
+    },
+    2: {
+      name : "Pizza Cantos",
+      desc : "Lorem ipsum dolor sit amet, constetur",
+      img : "pizza-cantos.svg",
+      price : 150
+    }
+};
+
+const cart = [
+    
+];
+
+const totalPrice = 0;
+
+const addtocart = document.querySelectorAll('.add-to-cart')
+
+addtocart.forEach((element) => {
+    element.addEventListener('click', (el) => {
+        let productId = element.getAttribute('product-id');
+        // totalPrice += products[productId].price;
+        // totalPrice += 10;
+        // console.log(totalPrice);
+        cart.push(products[productId])
+        console.log(cart)
+    })
+})
